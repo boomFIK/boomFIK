93a35547eab39b84de9fa6986a253f8599e2fa55
boomFIK <101659633+boomFIK@users.noreply.github.com>
2022-03-29 19:38:40 +0300

    Add files via upload

--- виселица.py
+++ виселица.py
@@ -1,4 +1,5 @@
 def game():
+    
     progress = True
     word = ['orange']
     lifes = 3
@@ -8,8 +9,71 @@ def game():
     template = start_template(word_in_play)
     welcome_speach(list_to_string_convert(template))
     while progress:
-        user_guess = user_input
+        user_guess = user_input()
         template = build_template(template, word_in_play, user_guess)
+        guessed = list_to_string_convert(template)
+        print_result(guessed)
+        progress = check_win(guessed)
+
+        if not check_mistake(word_in_play, user_guess):
+            print(f'Осталось {lifes} попытки')
+            lifes = check_attempt(lifes)
+
+        if lifes == 0:
+            lose_speach()
+            break
+
+        if not progress:
+            win_speech()
+
+def lose_speech():
+    print('вы проиграли!')
+
+def win_speech():
+    print('вы выйграли!')
+
+def check_attempt(life):
+    """
+input: life - int
+output: int, life -= 1
+"""
+    return lifes - 1
+    
+
+
+def check_mistake(w, g):
+    """
+input: w - word_in_play (string), g - user_input (string)
+    output: bool, checks if user guess is wrong
+    if no such g in w, return False, else True
+    """
+    
+    flag = False
+    for i in w:
+        flag = True
+    return flag
+  
+
+            
+            
+
+def check_win(g):
+    """
+input: g - template (string)
+output: bool, if no '_' in g return False, else True
+"""
+    for l in g:
+        if l == '_':
+            return True
+    return False
+
+def print_result(g):
+    """
+input: g - template (string)
+output: retorn None, used as just built-in function print(g)
+"""
+    print(f'Результат: {g}')
+    
 
 def build_template(t, w, g=''):
     """
@@ -78,9 +142,6 @@ TODO:  random string from list
 """
     return w[0]
 game()
-
-
-
 
 
 
